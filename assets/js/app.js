b86643065464ec8fcfddd06426ba52661ec9166c
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2020-10-06 16:41:42 -0600

    labels still not working

--- assets/js/app.js
+++ assets/js/app.js
@@ -73,19 +73,19 @@ d3.csv("assets/data/data.csv").then(function(data){
             .attr("text-anchor", "middle")
             .attr("fill", "white");
     
-    // // Create axes labels
-    // chartGroup.append("text")
-    //     .attr("transform", "rotate(-90)")
-    //     .attr("y", 0 - margin.left + 40)
-    //     .attr("x", 0 - (height / 2))
-    //     .attr("dy", "1em")
-    //     .attr("class", "axisText")
-    //     .text("Lacks Healthcare (%)");
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - ChartMargin.left + 40)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
     
-    // chartGroup.append("text")
-    //     .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-    //     .attr("class", "axisText")
-    //     .text("In Poverty (%)");
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");
 
 }).catch(function(error){
     console.log(error);
