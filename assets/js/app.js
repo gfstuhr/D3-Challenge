37f8ad24270e8358cf9c9321e725ff3b16b2719e
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2020-10-07 11:09:59 -0600

    Axes labels sort of working.

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,13 +27,16 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-d3.csv("assets/data/data.csv").then(function(data){
+// Open Data
+    d3.csv("assets/data/data.csv").then(function(data){
 
+    // Convert Data from strings to numbers
     data.forEach(function(d){
         d.poverty = + d.poverty;
         d.healthcareLow = + d.healthcareLow;
     });
 
+    // Linear scales for each axis
     var xLinearScale = d3.scaleLinear()
         .domain([d3.min(data, d => d.poverty-1),d3.max(data, d => d.poverty)+1])
         .range([0,chartWidth]);
@@ -42,9 +45,12 @@ d3.csv("assets/data/data.csv").then(function(data){
         .domain([d3.min(data, d => d.healthcareLow)-1, d3.max(data, d => d.healthcareLow)+1])
         .range([chartHeight, 0])
     
+    // Set x and y axis
     var x_axis = d3.axisBottom(xLinearScale);
     var y_axis = d3.axisLeft(yLinearScale).ticks(10);
 
+
+    // Drawing each axis
     chartGroup.append("g")
         .call(y_axis)
     
@@ -52,6 +58,7 @@ d3.csv("assets/data/data.csv").then(function(data){
         .attr("transform", `translate(0, ${chartHeight} )`)
         .call(x_axis);
 
+    // Plotting data
     chartGroup.selectAll("dot")
         .data(data)
         .enter()
@@ -60,7 +67,7 @@ d3.csv("assets/data/data.csv").then(function(data){
             .attr("cx", d=> xLinearScale(d.poverty))
             .attr("cy", d=> yLinearScale(d.healthcareLow))
             .classed("stateCircle",true)
-        
+    //Adding Plot Text 
     chartGroup.selectAll("dot")
         .data(data)
         .enter()
@@ -68,24 +75,31 @@ d3.csv("assets/data/data.csv").then(function(data){
             .attr("x", d=>xLinearScale(d.poverty))
             .attr("y", d=>yLinearScale(d.healthcareLow))
             .text(d => d.abbr)
-            .attr("font-family", "sans-serif")
             .attr("font-size", "10px")
             .attr("text-anchor", "middle")
             .attr("fill", "white");
     
     // Create axes labels
-    chartGroup.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - ChartMargin.left + 40)
-        .attr("x", 0 - (chartHeight / 2))
-        .attr("dy", "1em")
-        .attr("class", "axisText")
-        .text("Lacks Healthcare (%)");
-    
-    chartGroup.append("text")
-        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
-        .attr("class", "axisText")
+
+    var x_labels = chartGroup.append("g")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top })`)
+
+    var povertylabel = x_labels.append("text")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("value", "poverty") 
+        .classed("active", true)
         .text("In Poverty (%)");
+    
+    var y_labels = chartGroup.append("g")
+        .attr("transform", "rotate(-90)")
+        
+    var healthcare_label=y_labels.append("text")
+        .attr("x", 0 - (chartHeight/2))
+        .attr("y", chartMargin.left - 5 )
+        .attr("value", "healthcare") 
+        .classed("active", true)
+        .text("Lacks Healthcare (%)");
 
 }).catch(function(error){
     console.log(error);
